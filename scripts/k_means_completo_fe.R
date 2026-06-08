a746703fa924c5050a408932ebabf9e550ba49b7
Denis-Yen <denisyenrc@gmail.com>
2026-06-08 17:12:17 -0500

    Mapa dpedencia

--- scripts/k_means_completo_fe.R
+++ scripts/k_means_completo_fe.R
@@ -56,7 +56,7 @@ k_means <- kmeans(dependencia_tidy |> select(TDD_JOVE, TDD_VEJEZ, RA_POTENCIAL, 
 fviz_cluster(k_means,
              data = dependencia_tidy |> select(TDD_JOVE, TDD_VEJEZ, RA_POTENCIAL, RA_PADRES),
              geom = "point",
-             palette = c("#C1121F", "#8D99AE", "#000000", "#B1A7A6", "#D90429"),
+             palette = c("#6b2328", "#de6a73", "#f5d2d5", "#e68f96", "#4b1217"),
              ggtheme = theme_minimal(),
              repel = TRUE
              ) +
@@ -106,7 +106,7 @@ dependencia_tidy_clust_fe %>%
     TD_VEJ = mean(TDD_VEJEZ),
     TD_POT = mean(RA_POTENCIAL),
     TD_PAD = mean(RA_PADRES),
-  ) 
+  )
 
 
 # 5. VISUALIZACIÓN 3D CON PLOTLY -----------------------------------------
@@ -139,12 +139,76 @@ fig <- plot_ly(
 
 fig
 
+# 6. MAPA DE CLUSTERS -----------------------------------------------------
+library(sf)
+shp_peru <- read_sf("data/Peru_Distrital.geojson")
+clus_peru <- dependencia_tidy_clust_fe
+
+clus_shp_peru <- shp_peru |> left_join(clus_peru,by = c("UBIGEO"="UBIGEO"))
+
+paleta_clusters_info <- c(
+  "1" = "#6b2328",
+  "2" = "#e68f96",
+  "3" = "#f5d2d5",
+  "4" = "#de6a73",
+  "5" = "#4b1217"
+)
 
 
+clus_shp_peru |> ggplot() +
+  geom_sf(aes(fill = factor(CLUSTER)),
+          color = "white",
+          linewidth = 0.01
+          ) +
+  scale_fill_manual(
+            values = paleta_clusters_info,
+            name = "Cluster") +
+  labs(
+    title = stringr::str_trim("Perú: Clusters de Dependencia Demográfica"),
+    subtitle = "(n = 1,891)",
+    caption = "Fuente: MINSA. Repositorio Único Nacional de Información en Salud (REUNIS)*.\nElaboración: Denis Rodríguez (www.denis-rodriguez.com)")+
+  guides(fill=guide_legend(
+    direction = "vertical",
+    keyheight = unit(4, "mm"),
+    keywidth = unit(6, "mm"),
+    title.position = 'top',
+    title.hjust = 0.5,
+    label.hjust = .5,
+    reverse = F,
+    label.position = "right"
+  ))+ 
+  # geom_text(
+  # data = cents |> filter(Tasa_incidencia >= 240), aes(coords.x1, coords.x2, label = paste0(NOMBPROV, "\n", round(Tasa_incidencia, 1))
+  # ),
+  # size = 3,
+  # fontface = "bold",
+  # color = "#000000",
+  # family = "georg") +
+  theme(
+    panel.background = element_blank(), 
+    legend.background = element_blank(),
+    legend.position = "right",
+    panel.border = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_blank(),
+    plot.title = element_text(size=22, color='#05204d', hjust=0.3, vjust=1, face = "bold"),
+    plot.subtitle = element_text(size=18, color='#4b1217', hjust=0.5, vjust=-1, face = "bold"),
+    plot.caption = element_text(size=11, color="grey60", hjust=0.0, vjust=-1, lineheight = 0.8),
+    axis.title.x = element_text(size=18, color="grey20", hjust=0.5, vjust=-6),
+    legend.text = element_text(size=12, color="grey20"),
+    legend.title = element_text(size=12, color="grey20"),
+    strip.text = element_text(size=12),
+    plot.margin = unit(c(t=1, r=-2, b=1, l=-2),"lines"),
+    axis.title.y = element_blank(),
+    axis.ticks = element_blank(),
+    axis.text.x = element_blank(),
+    axis.text.y = element_blank()) +
+  labs(x ="") -> map_nive_dist_clust
+ 
+ggsave(plot = map_nive_dist_clust, "imagenes/map_nive_dist_clus.png", width = 2500, height = 2000, units = "px")
 
 
 dependencia_tidy_clust_fe%>% write.xlsx("salidas/depedencia_cluster_fe.xlsx")
-
 
 
 
